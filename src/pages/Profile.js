ea2a80d2ac00d263fac60c30b932d88b6b9d3ca1
dalmasonto <49430208+dalmasonto@users.noreply.github.com>
2020-08-13 18:32:35 +0300

    Update Profile.js

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Navbar from '../components/Nav'
 
 function Profile() {
   return (
-    <div>
+    <
+<Navbar page='Profile' />
       <h4> Profile </h4>
-    </div>
+    </>
   )
 }
 
